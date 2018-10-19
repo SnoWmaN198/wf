7c6ec303835d535f27313a5c0efdd2ff0f000271
SnoWmaN198 <goncalves.ricardo_88@hotmail.fr>
2018-10-19 12:40:06 +0200

    comments

--- PHP/Exercice3/exercise.php
+++ PHP/Exercice3/exercise.php
@@ -7,6 +7,9 @@ $playerACount = 0;
 $playerBCount = 0;
 
 foreach($input as $value) {
+   /* list ($a, $b) = $value;       // list is used to separate the elements inside the array (same as $value[0], $value[1])
+    if ($a < $b){} */               // better execution time than ($value[0], $value[1])  
+
     if ($value[0] < $value[1]) {
         $playerBCount ++;
     } else if ($value[0] > $value[1]) {
