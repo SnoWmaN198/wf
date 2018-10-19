c05e9070940c62e1c76e2f7fe23438291a823580
SnoWmaN198 <goncalves.ricardo_88@hotmail.fr>
2018-10-19 10:48:36 +0200

    ex2

--- PHP/Exercice2/exercise.php
+++ PHP/Exercice2/exercise.php
@@ -1,5 +1,10 @@
 <?php
-$password;
-$salt;
+$password = 123456789;
+$salt = 'S';
 
-$saltedPassword = '';
+$firstPart = substr($password, 0, floor(strlen($password)/2) + (strlen($password) % 2));   // modulo (%) is used to distinct between even and odd lengths  
+$lastPart = substr($password, ceil(strlen($password)/2));
+
+$saltedPassword = $firstPart.$salt.$lastPart;
+
+echo $saltedPassword;
