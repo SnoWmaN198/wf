7a3dbb9e61e08715efb486adf4ed00da4aaaf1b1
SnoWmaN198 <goncalves.ricardo_88@hotmail.fr>
2018-10-19 12:31:54 +0200

    ex3

--- PHP/Exercice3/exercise.php
+++ PHP/Exercice3/exercise.php
@@ -2,3 +2,22 @@
 
 $input;
 $winner;
+
+$playerACount = 0;
+$playerBCount = 0;
+
+foreach($input as $value) {
+    if ($value[0] < $value[1]) {
+        $playerBCount ++;
+    } else if ($value[0] > $value[1]) {
+        $playerACount ++;
+    }
+}
+
+if ($playerACount < $playerBCount) {
+    $winner = 'B';
+} else if ($playerACount > $playerBCount) {
+    $winner = 'A';
+}
+
+//echo $winner;
