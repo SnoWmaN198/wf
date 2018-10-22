d74fa5adcf8d2373482e27dfaeeb44b127f47910
SnoWmaN198 <goncalves.ricardo_88@hotmail.fr>
2018-10-22 14:45:08 +0200

    Ex6

--- PHP/Exercice6/exercise.php
+++ PHP/Exercice6/exercise.php
@@ -1,1 +1,23 @@
 <?php
+
+// create a function with filepath as parameter
+function easterReverse($filePath){
+    // get the full file content
+    $content = file_get_contents($filePath);
+    
+    // divide the file content by 2
+    $secondPart = substr($content, floor(strlen($content) / 2));
+    $firstPart = substr($content, 0, strlen($secondPart) - 1);
+    
+    // open the file in writing mode
+    $file = fopen($filePath, 'r+');
+    
+    // move the cursor to the first content part 
+    fseek($file, strlen($firstPart), SEEK_SET);
+    
+    // write the reversed second part into the file (strrev)
+    fwrite($file, strrev($secondPart), strlen($secondPart));
+    
+    // close the file
+    fclose($file);
+}
