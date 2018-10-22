656f552c2699fcba6c69b0b9a3de0fe8b2b9be95
SnoWmaN198 <goncalves.ricardo_88@hotmail.fr>
2018-10-22 17:01:53 +0200

    Ex7

--- PHP/Exercice7/exercise.php
+++ PHP/Exercice7/exercise.php
@@ -1,1 +1,20 @@
 <?php
+
+function divide(int $value, int $divider) : float {
+    if ($divider == 0) {
+        throw new RuntimeException('Division by 0 is not allowed');
+    }
+    return ($value/$divider);
+}
+
+function arrayDivide(array $value, int $divider) : array {
+    $result = [];
+    foreach ($value as $base){
+        try {
+            $result[] = divide($base, $divider);
+        } catch (RuntimeException $e) {
+            $result[] = $base;
+        }
+    }
+    return $result;
+}
