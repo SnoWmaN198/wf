c544da0a23a75fe218c801dd1621b55d2988edf4
SnoWmaN198 <goncalves.ricardo_88@hotmail.fr>
2018-10-25 10:33:32 +0200

    Model and Public folder separation

--- PHP/My Homepage/public/index.php
+++ PHP/My Homepage/public/index.php
@@ -1,22 +1,3 @@
-<?php 
-    //Articles : id, pub_date, img, title, description
-    $articles = [
-        [
-            'id' => 1,
-            'pub_date' => '2018-06-21 11:43:12',
-            'img' => 'https://picsum.photos/100/100/?random',
-            'title' => 'Title 1',
-            'description' => 'Lorem ipsum sit amet'
-        ],
-        [
-            'id' => 2,
-            'pub_date' => '2018-06-22 09:33:17',
-            'img' => 'https://picsum.photos/100/100/?random',
-            'title' => 'Title 2',
-            'description' => 'Lorem ipsum sit amet et net'
-        ]
-    ];
-?>
 <!DOCTYPE html>
 <html>
     <head>
