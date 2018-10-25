9d23bf6ea540d9c75dee934015b3990f8cb26677
SnoWmaN198 <goncalves.ricardo_88@hotmail.fr>
2018-10-25 10:40:23 +0200

    View

---
+++ PHP/My Homepage/View/ArticlesList.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset= "UTF-8">
+        <title>Tag be sill home page</title>
+    </head>
+    <body>
+        <h1>Tag be sill</h1>
+        
+        <p>
+        	The actuel time is 
+            <?php 
+            
+            echo (new DateTime())->format('Y-m-d H:i:s');
+            
+            ?>
+        </p>
+        <table border>
+        		<?php 
+        		echo '<tr>'; 
+        		echo '<th> </th>';
+        		foreach ($articles[0] as $key=>$header){
+        		    if($key != 'img'){ 
+        		        echo "<th> $key </th>"; 
+        		    }
+        		}
+                echo '</tr>';
+                
+        		foreach ($articles as $article){
+        		    echo '<tr>';
+        		    echo "<td><img src=' ".$article['img']."'/>"."</td>";
+        		    echo "<td> ".$article['id']." </td>";
+        		    echo "<td> ".$article['pub_date']." </td>";
+        		    echo "<td> ".$article['title']." </td>";
+        		    echo "<td> ".$article['description']." </td>";
+        		    echo '</tr>';
+        		}
+        		
+        		?> 		
+        </table>
+    </body>
+</html>
